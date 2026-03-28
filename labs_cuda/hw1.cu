35b817d78242843effe1cbfec57a1a352f91bd98
kalane15 <kalane15@yandex.ru>
2026-03-28 11:36:28 +0300

    фы

---
+++ labs_cuda/hw1.cu
@@ -0,0 +1,39 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    float a, b, c;
+    std::cin >> a >> b >> c;
+
+    std::cout.precision(6);
+    std::cout << std::fixed;
+
+    if (a == 0) {
+        if (b == 0) {
+            if (c == 0) {
+                std::cout << "any";
+                return;
+            }
+            std::cout << "incorrect";
+            return;
+        }
+        std::cout << -c / b;
+        return;
+    }
+
+    float d = b * b - 4 * a * c;
+    if (d < 0.0) {
+        std::cout << "imaginary";
+        return;
+    }
+
+    float x1, x2;
+    x1 = (-b + sqrt(d)) / (2 * a);
+    x2 = (-b - sqrt(d)) / (2 * a);
+
+    std::cout << x1 << " " << x2;
+}
---
+++ labs_cuda/hw2.cu
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <iomanip>
+
+void bubble_sort(int n, float* arr) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                float temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    float* arr = new float[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+    bubble_sort(n, arr);
+    for (int i = 0; i < n; i++) {
+        std::cout << std::scientific << std::setprecision(6) << arr[i];
+        if (i < n - 1) {
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+    delete[] arr;
+    return 0;
+}
